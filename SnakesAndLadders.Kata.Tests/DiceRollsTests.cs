9665e02e6a5b7c09655bfd33bda15869dfff7831
Antonios Klimis <antonios.klimis@gmail.com>
2018-11-11 19:48:21 +0200

    Extracted IDiceRoller Interface; Created a mock to produce a dice roll of 4;

--- SnakesAndLadders.Kata.Tests/DiceRollsTests.cs
+++ SnakesAndLadders.Kata.Tests/DiceRollsTests.cs
@@ -1,5 +1,6 @@
 using System;
 using FluentAssertions;
+using Moq;
 using Xunit;
 
 namespace SnakesAndLadders.Kata.Tests
@@ -10,7 +11,7 @@ namespace SnakesAndLadders.Kata.Tests
         public void GivenGameIsStarted_WhenPlayerRollsADie_ThenResultShouldBeBetweenOneAndSixInclusive()
         {
             // Arrange
-            var validDices= new[] {1, 2, 3, 4, 5, 6};
+            var validDices = new[] {1, 2, 3, 4, 5, 6};
             var diceRoller = new DiceRoller();
 
             // Act
@@ -19,9 +20,46 @@ namespace SnakesAndLadders.Kata.Tests
             // Assert
             dice.Should().BeOneOf(validDices);
         }
+
+        [Fact]
+        public void GivenPlayerRollsAFour_WhenMoveToke_ThenTokenSquareShouldBeFour()
+        {
+            // Arrange
+            var token = new Token();
+            var player = new Player(token);
+
+            var diceRollerMock = new Mock<IDiceRoller>();
+            diceRollerMock.Setup(roller => roller.Roll()).Returns(4);
+            
+            // Act
+            player.MoveToken(diceRollerMock.Object);
+
+            // Assert
+            token.Square.Should().Be(5);
+        }
     }
 
-    public class DiceRoller
+    public class Player
+    {
+        private readonly Token _token;
+
+        public Player(Token token)
+        {
+            _token = token;
+        }
+
+        public void MoveToken(IDiceRoller diceRoller)
+        {
+            _token.Move(diceRoller.Roll());
+        }
+    }
+
+    public interface IDiceRoller
+    {
+        int Roll();
+    }
+
+    public class DiceRoller : IDiceRoller
     {
         private readonly Random _random = new Random();
 
